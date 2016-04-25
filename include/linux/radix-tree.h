79ca66e11936232362d8ed83c97d2055ed669ad4
Matthew Wilcox <willy@linux.intel.com>
2016-04-25 15:08:20 -0400

    radix-tree: Add radix_tree_split
    This new function splits a larger multiorder entry into smaller entries
    (potentially multi-order entries).  These entries are initialised to
    RADIX_TREE_RETRY to ensure that RCU walkers who see this state aren't
    confused.  The caller should then call radix_tree_for_each_slot() and
    radix_tree_replace_slot() in order to turn these retry entries into the
    intended new entries.  Tags are replicated from the original multiorder
    entry into each new entry.
    
    Signed-off-by: Matthew Wilcox <willy@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -319,8 +319,11 @@ static inline void radix_tree_preload_end(void)
 	preempt_enable();
 }
 
+int radix_tree_split(struct radix_tree_root *, unsigned long index,
+			unsigned new_order);
 int radix_tree_join(struct radix_tree_root *, unsigned long index,
 			unsigned new_order, void *);
+
 /**
  * struct radix_tree_iter - radix tree iterator state
  *
