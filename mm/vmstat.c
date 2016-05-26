ce69c0116b23acb232902ae3e6e5e2099786e51d
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2016-05-26 13:27:17 +0300

    mm, rmap: account file thp pages
    Let's add FileHugePages and FilePmdMapped fields into meminfo and smaps.
    It indicates how many times we allocate and map file THP.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -967,6 +967,8 @@ const char * const vmstat_text[] = {
 	"nr_shmem",
 	"nr_shmem_hugepages",
 	"nr_shmem_pmdmapped",
+	"nr_file_hugepaged",
+	"nr_file_pmdmapped",
 	"nr_anon_transparent_hugepages",
 	"nr_unstable",
 	"nr_vmscan_write",
