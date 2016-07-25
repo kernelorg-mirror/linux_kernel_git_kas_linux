30ebc74559f89a0d6014b3a477b15ab0865ba709
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2016-07-25 13:07:59 +0300

    ext4: handle huge pages in __ext4_block_zero_page_range()
    As the function handles zeroing range only within one block, the
    required changes are trivial, just remove assuption on page size.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3685,7 +3685,7 @@ static int __ext4_block_zero_page_range(handle_t *handle,
 		struct address_space *mapping, loff_t from, loff_t length)
 {
 	ext4_fsblk_t index = from >> PAGE_SHIFT;
-	unsigned offset = from & (PAGE_SIZE-1);
+	unsigned offset;
 	unsigned blocksize, pos;
 	ext4_lblk_t iblock;
 	struct inode *inode = mapping->host;
@@ -3697,6 +3697,9 @@ static int __ext4_block_zero_page_range(handle_t *handle,
 				   mapping_gfp_constraint(mapping, ~__GFP_FS));
 	if (!page)
 		return -ENOMEM;
+
+	page = compound_head(page);
+	offset = from & ~hpage_mask(page);
 
 	blocksize = inode->i_sb->s_blocksize;
 
@@ -3752,7 +3755,7 @@ static int __ext4_block_zero_page_range(handle_t *handle,
 		if (err)
 			goto unlock;
 	}
-	zero_user(page, offset, length);
+	zero_user(page + offset / PAGE_SIZE, offset % PAGE_SIZE, length);
 	BUFFER_TRACE(bh, "zeroed end of block");
 
 	if (ext4_should_journal_data(inode)) {
