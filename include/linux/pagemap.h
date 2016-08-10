47e4e0e413a2ceea81588d95b5480736e012e47a
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2016-08-10 00:54:14 +0000

    mm, hugetlb: switch hugetlbfs to multi-order radix-tree entries
    Currently, hugetlb pages are linked to page cache on the basis of hugepage
    offset (derived from vma_hugecache_offset()) for historical reason, which
    doesn't match to the generic usage of page cache and requires some routines
    to covert page offset <=> hugepage offset in common path. This patch
    adjusts code for multi-order radix-tree to avoid the situation.
    
    Main change is on the behavior of page->index for hugetlbfs. Before this
    patch, it represented hugepage offset, but with this patch it represents
    page offset. So index-related code have to be updated.
    Note that hugetlb_fault_mutex_hash() and reservation region handling are
    still working with hugepage offset.
    
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    [kirill.shutemov@linux.intel.com: reject fixed]
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -380,14 +380,10 @@ static inline struct page *read_mapping_page(struct address_space *mapping,
 
 /*
  * Get the offset in PAGE_SIZE.
- * (TODO: hugepage should have ->index in PAGE_SIZE)
  */
 static inline pgoff_t page_to_pgoff(struct page *page)
 {
 	pgoff_t pgoff;
-
-	if (unlikely(PageHeadHuge(page)))
-		return page->index << compound_order(page);
 
 	if (likely(!PageTransTail(page)))
 		return page->index;
@@ -414,15 +410,11 @@ static inline loff_t page_file_offset(struct page *page)
 	return ((loff_t)page_file_index(page)) << PAGE_SHIFT;
 }
 
-extern pgoff_t linear_hugepage_index(struct vm_area_struct *vma,
-				     unsigned long address);
-
 static inline pgoff_t linear_page_index(struct vm_area_struct *vma,
 					unsigned long address)
 {
 	pgoff_t pgoff;
-	if (unlikely(is_vm_hugetlb_page(vma)))
-		return linear_hugepage_index(vma, address);
+
 	pgoff = (address - vma->vm_start) >> PAGE_SHIFT;
 	pgoff += vma->vm_pgoff;
 	return pgoff;
