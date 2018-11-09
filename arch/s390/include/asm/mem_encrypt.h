9939274729fe42c63927c85c5089d0c44283ded9
Jacob Pan <jacob.jun.pan@linux.intel.com>
2018-11-09 10:03:12 -0800

    x86/mm: introduce common code for mem encryption
    Both Intel MKTME and AMD SME have needs to support DMA address
    translation with encryption related bits. Common functions are
    introduced in this patch to keep DMA generic code abstracted.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/s390/include/asm/mem_encrypt.h
+++ arch/s390/include/asm/mem_encrypt.h
@@ -12,6 +12,16 @@ extern bool sev_active(void);
 int set_memory_encrypted(unsigned long addr, int numpages);
 int set_memory_decrypted(unsigned long addr, int numpages);
 
+static inline dma_addr_t __mem_encrypt_dma_set(dma_addr_t daddr, phys_addr_t paddr)
+{
+	return daddr;
+}
+
+static inline phys_addr_t __mem_encrypt_dma_clear(phys_addr_t paddr)
+{
+	return paddr;
+}
+
 #endif	/* __ASSEMBLY__ */
 
 #endif	/* S390_MEM_ENCRYPT_H__ */
