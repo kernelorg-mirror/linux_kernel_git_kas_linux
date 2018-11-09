f04dacdedc5cf22fe63ce0c8a151de3a27e49746
Jacob Pan <jacob.jun.pan@linux.intel.com>
2018-11-09 10:03:11 -0800

    iommu/vt-d: Support MKTME in DMA remapping
    When MKTME is enabled, keyid is stored in the high order bits of physical
    address. For DMA transactions targeting encrypted physical memory, keyid
    must be included in the IOVA to physical address translation.
    
    This patch appends page keyid when setting up the IOMMU PTEs. On the
    reverse direction, keyid bits are cleared in the physical address lookup.
    Mapping functions of both DMA ops and IOMMU ops are covered.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- include/linux/intel-iommu.h
+++ include/linux/intel-iommu.h
@@ -22,6 +22,7 @@
 
 #include <asm/cacheflush.h>
 #include <asm/iommu.h>
+#include <asm/mktme.h>
 
 /*
  * VT-d hardware uses 4KiB page size regardless of host page size.
@@ -605,7 +606,12 @@ static inline void dma_clear_pte(struct dma_pte *pte)
 static inline u64 dma_pte_addr(struct dma_pte *pte)
 {
 #ifdef CONFIG_64BIT
-	return pte->val & VTD_PAGE_MASK;
+	u64 addr = pte->val;
+	addr &= VTD_PAGE_MASK;
+#ifdef CONFIG_X86_INTEL_MKTME
+	addr &= ~mktme_keyid_mask();
+#endif
+	return addr;
 #else
 	/* Must have a full atomic 64-bit read */
 	return  __cmpxchg64(&pte->val, 0ULL, 0ULL) & VTD_PAGE_MASK;
