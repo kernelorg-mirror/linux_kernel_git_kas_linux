506f08b0ab2ed955471883984afc787f43dd4607
Alison Schofield <alison.schofield@intel.com>
2019-07-19 16:43:57 -0700

    keys/mktme: Require CAP_SYS_RESOURCE capability for MKTME keys
    The MKTME key type uses capabilities to restrict the allocation
    of keys to privileged users. CAP_SYS_RESOURCE is required, but
    the broader capability of CAP_SYS_ADMIN is accepted.
    
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- security/keys/mktme_keys.c
+++ security/keys/mktme_keys.c
@@ -2,6 +2,7 @@
 
 /* Documentation/x86/mktme/ */
 
+#include <linux/cred.h>
 #include <linux/cpu.h>
 #include <linux/init.h>
 #include <linux/key.h>
@@ -370,6 +371,9 @@ int mktme_preparse_payload(struct key_preparsed_payload *prep)
 	u32 *mktme_payload;
 	char *options;
 	int ret;
+
+	if (!capable(CAP_SYS_RESOURCE) && !capable(CAP_SYS_ADMIN))
+		return -EACCES;
 
 	if (datalen <= 0 || datalen > 1024 || !prep->data)
 		return -EINVAL;
